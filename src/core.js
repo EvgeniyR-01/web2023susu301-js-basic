afdfacbc4d6d6dcb0d3608d4998dd5e5c0c28c98
EvgeniyR-01 <ryavkin.a11122@gmail.com>
2023-10-17 13:57:00 +0500

    functions are written and tested; node updated from 2.1.3 to 2.3.2

--- src/core.js
+++ src/core.js
@@ -1,23 +1,80 @@
 //Напишите функцию, которая проверяет, является ли число целым используя побитовые операторы
-function isInteger(n) {}
+function isInteger(n) {
+    return (n ^ 0) === n;
+}
 
 //Напишите функцию, которая возвращает массив четных чисел от 2 до 20 включительно
-function even() {}
+function even() {
+    let buf = [];
+    for (let i = 2; i <= 20; i += 2) {
+        buf.push(i);
+    }
+    return buf;
+}
 
 //Напишите функцию, считающую сумму чисел до заданного используя цикл
-function sumTo(n) {}
+function sumTo(n) {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
+}
 
 //Напишите функцию, считающую сумму чисел до заданного используя рекурсию
-function recSumTo(n) {}
+function recSumTo(n) {
+    if (n < 1) {
+        throw new Error('n must not be less than 1');
+    }
+
+    if (n === 1) {
+        return 1;
+    } else {
+        return n + recSumTo(n - 1);
+    }
+}
 
 //Напишите функцию, считающую факториал заданного числа
-function factorial(n) {}
+function factorial(n) {
+    if (n < 0) {
+        throw new Error('n must be non-negative');
+    }
+    if (n === 0) {
+        return 1;
+    }
+
+    let result = 1;
+    for (let i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
 
 //Напишите функцию, которая определяет, является ли число двойкой, возведенной в степень
-function isBinary(n) {}
+function isBinary(n) {
+    let isBin = false;
+    let degree = Math.log(n) / Math.log(2);
+    if (degree % 1 === 0) {
+        isBin = true;
+    } else {
+        isBin = false;
+    }
+    return isBin;
+}
 
 //Напишите функцию, которая находит N-е число Фибоначчи
-function fibonacci(n) {}
+function fibonacci(n) {
+    if (n < 0) {
+        throw new Error('n must be non-negative');
+    }
+
+    if (n === 0) {
+        return 0;
+    } else if (n === 1) {
+        return 1;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
 
 /** Напишите функцию, которая принимает начальное значение и функцию операции
  * и возвращает функцию - выполняющую эту операцию.
@@ -30,7 +87,18 @@ function fibonacci(n) {}
  * console.log(sumFn(5)) - 15
  * console.log(sumFn(3)) - 18
  */
-function getOperationFn(initialValue, operatorFn) {}
+function getOperationFn(initialValue, operatorFn) {
+    let storedValue = initialValue;
+
+    if (!operatorFn) {
+        return () => storedValue;
+    }
+
+    return (newValue) => {
+        storedValue = operatorFn(storedValue, newValue);
+        return storedValue;
+    };
+}
 
 /**
  * Напишите функцию создания генератора арифметической последовательности.
@@ -48,7 +116,14 @@ function getOperationFn(initialValue, operatorFn) {}
  * console.log(generator()); // 7
  * console.log(generator()); // 9
  */
-function sequence(start, step) {}
+function sequence(start = 0, step = 1) {
+    const generator = function () {
+        const oldValue = start;
+        start += step;
+        return oldValue;
+    };
+    return generator;
+}
 
 /**
  * Напишите функцию deepEqual, которая принимает два значения
@@ -64,7 +139,42 @@ function sequence(start, step) {}
  * deepEqual({arr: [22, 33], text: 'text'}, {arr: [22, 33], text: 'text'}) // true
  * deepEqual({arr: [22, 33], text: 'text'}, {arr: [22, 3], text: 'text2'}) // false
  */
-function deepEqual(firstObject, secondObject) {}
+function deepEqual(firstObject, secondObject) {
+    if (firstObject === null && secondObject === null) {
+        return true;
+    } else if (firstObject === undefined && secondObject === undefined) {
+        return true;
+    } else if (Number.isNaN(firstObject) && Number.isNaN(secondObject)) {
+        return true;
+    } else if (firstObject === null || secondObject === null) {
+        return false;
+    } else if (firstObject === undefined || secondObject === undefined) {
+        return false;
+    } else if (Number.isNaN(firstObject) || Number.isNaN(secondObject)) {
+        return false;
+    }
+
+    if (typeof firstObject !== 'object' && typeof secondObject !== 'object') {
+        return firstObject === secondObject;
+    }
+
+    let firstKeys = Object.keys(firstObject);
+    let secondKeys = Object.keys(secondObject);
+
+    if (firstKeys.length !== secondKeys.length) {
+        return false;
+    }
+
+    for (let key of firstKeys) {
+        if (!secondKeys.includes(key)) {
+            return false;
+        }
+        if (!deepEqual(firstObject[key], secondObject[key])) {
+            return false;
+        }
+    }
+    return true;
+}
 
 module.exports = {
     isInteger,
